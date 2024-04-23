0d6ae161363a63c408b7505e3ac7da551e51827f
Michał Czubaj <czubaij@gmail.com>
2024-04-23 22:41:58 +0200

    project reanimation, set up router for dayz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css'
-import Game from './Game'
+import Game from './czubordle/Game/Game'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+const HomeMenu = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/czubordle">czubordle</Link>
+      </li>
+      <li>
+        <Link to="/dayzrecipes">dayzrecipes</Link>
+      </li>
+    </ul>
+  </div>
+)
 
 const App = () => (
-  <div>
-    <header>CZUBORDLE</header>
-    <Game />
-  </div>
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<HomeMenu />} />
+      <Route path="/czubordle" element={<Game />} />
+      <Route path="/dayzrecipes"></Route>
+    </Routes>
+  </Router>
 )
 
 export default App
